2a11f7d75af9760478614658c0de132866292cec
bennny2 <benhurley01@outlook.com>
2024-01-07 13:50:46 +0000

    updated letters array
    changed from one of each letter to scrabble letter frequencies

--- Assets/Scripts/Letter.cs
+++ Assets/Scripts/Letter.cs
@@ -6,8 +6,35 @@ using UnityEngine;
 public static class Letter
 {
     //list of all letters to be pulled from to put a letter in a hex
-    private static readonly char[] Letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
+private static readonly char[] Letters =
+    {
+        'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',
+        'b', 'b',
+        'c', 'c',
+        'd', 'd', 'd', 'd',
+        'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e',
+        'f', 'f',
+        'g', 'g', 'g',
+        'h', 'h',
+        'i', 'i', 'i', 'i', 'i', 'i', 'i', 'i', 'i',
+        'j',
+        'k',
+        'l', 'l', 'l', 'l',
+        'm', 'm',
+        'n', 'n', 'n', 'n', 'n', 'n',
+        'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',
+        'p', 'p',
+        'q',
+        'r', 'r', 'r', 'r', 'r', 'r',
+        's', 's', 's', 's',
+        't', 't', 't', 't', 't', 't',
+        'u', 'u', 'u', 'u',
+        'v', 'v',
+        'w', 'w',
+        'x',
+        'y', 'y',
+        'z'
+    };
     public static char GenerateLetter(){ //pick random letter
         return Letters[Random.Range(0, Letters.Length)];
     }
