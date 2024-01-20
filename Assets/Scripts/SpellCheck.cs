a085232671453ab449ee78e317e731b31bf368d0
bennny2 <148907483+bennny2@users.noreply.github.com>
2024-01-20 17:40:16 +0000

    reformatting
    small changes to spacing naming etc

--- Assets/Scripts/SpellCheck.cs
+++ Assets/Scripts/SpellCheck.cs
@@ -29,27 +29,20 @@ public class SpellCheck
     }
 
     //Class Methods
-    public bool IsValidWord(string word)
-    {
+    public bool IsValidWord(string word) {
         return word.Length > 2 && spelling.TestWord(word);
     }
 
     private bool CanTheseLettersMakeAWord(List<string> letters) {
-
         IEnumerable<string> letterEnumerable = letters;
-        List<string> permutationsEnumerable = GetPermutations(letterEnumerable, 4)
-            .Select(perm => string.Join("", perm))
-            .ToList();
+        List<string> permutations = GetPermutations(letterEnumerable, 4).Select(perm => string.Join("", perm)).ToList();
 
-        foreach (string perm in permutationsEnumerable)
-        {
-            if (IsValidWord(perm))
-            {
-                Debug.Log(perm);
+        foreach (string potentialWord in permutations) {
+            if (IsValidWord(potentialWord)) {
+                Debug.Log(potentialWord);
                 return true;
             }
         }
-
         return false;
     }
 
@@ -60,19 +53,9 @@ public class SpellCheck
                 letters.Add(hex.HexagonText.text);
             }
         }
-
-        bool wordIsPlayable = CanTheseLettersMakeAWord(letters);
-
-        return wordIsPlayable;
+        return CanTheseLettersMakeAWord(letters);
     }
-
-    private IEnumerable<IEnumerable<T>> GetPermutationsWithRept<T>(IEnumerable<T> list, int length) {
-        if (length == 1) return list.Select(t => new T[] { t });
-        return GetPermutationsWithRept(list, length - 1)
-            .SelectMany(t => list, 
-                (t1, t2) => t1.Concat(new T[] { t2 }));
-    }
-
+    
     private IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length) {
         if (length == 1) {
             return list.Select(t => new T[] { t });
